464fe4b1baaca3b585e6d54c603a80825744bb74
Shekhar Prasad Rajak <shekharstudy@ymail.com>
2015-06-21 00:58:58 +0530

    adding menu_panel

---
+++ luna/SliderDemo/src/OvalPanel.java
@@ -0,0 +1,32 @@
+
+//  OvalPanel Class
+
+import java.awt.*;
+import javax.swing.*;
+
+public class OvalPanel extends JPanel {
+   private int diameter = 10;
+
+   public void paintComponent( Graphics g )
+   {
+      super.paintComponent( g );
+      g.fillOval( 10, 10, diameter, diameter );
+   }
+
+   public void setDiameter( int d )
+   {
+      diameter = ( d >= 0 ? d : 10 );  // default diameter 10
+      repaint();
+   }
+
+   // the following methods are used by layout managers
+   public Dimension getPreferredSize()
+   {
+      return new Dimension( 200, 200 );
+   }
+
+   public Dimension getMinimumSize()
+   {
+      return getPreferredSize();
+   }
+}
